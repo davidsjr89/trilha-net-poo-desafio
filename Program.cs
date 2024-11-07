ee6810b11d6c959df203c4323904207b94753580
David Junior <djunior@avipam.com.br>
2024-11-07 11:36:08 -0300

    Finalizado o exercicio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("(11)91111-1111", "Nokia A1", "fduioasjfdiosajio", 2);
+Iphone iphone = new Iphone("(11)92222-2222", "Iphone 2", "324532908kfdsçal", 1);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+Console.ReadKey();
